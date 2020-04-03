7ca747f37e02218efe78b5caf9a3f64973c394fe
fznx-io <mufahaza@gmail.com>
2020-04-03 11:15:00 +0700

    Configure environment variables

--- indexing.py
+++ indexing.py
@@ -5,4 +5,13 @@ from jira import JIRA
 from pymongo import MongoClient
 
 def indexing():
+    service = os.environ["SERVICE"]
+    admin_pwd = os.environ["ADMIN_PWD"]
+    database = os.environ["DATABASE"]
+    collection = os.environ["COLLECTION"]
+    new_index = os.environ["NEW_INDEX"]
+    issue_key = os.environ["ISSUE_KEY"]
+    jira_user = os.environ["ISSUE_KEY"]
+    jira_token = os.environ["JIRA_TOKEN"]
+
     pass
