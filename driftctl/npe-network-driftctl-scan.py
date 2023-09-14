2cb5b0c5df5208c36d70f246b82d22eae4c5c68b
fznx-io <mufahaza@gmail.com>
2023-09-14 11:36:21 +0700

    add npe scan

---
+++ driftctl/npe-network-driftctl-scan.py
@@ -0,0 +1,31 @@
+import json
+import random
+import sys
+import requests
+import os
+stream = os.popen('GOOGLE_APPLICATION_CREDENTIALS={GOOGLE_APPLICATION_CREDENTIALS} CLOUDSDK_CORE_PROJECT={GCP_PROJECT_NPE_NETWORK} driftctl scan --quiet  --to gcp+tf --from tfstate+gs://ts-cloudstorage-npe-terraform/terraform/npe/host/default.tfstate')
+output = stream.read()
+output
+
+def slack_notification_content():
+     slack_data = {
+         "username": "NPE NETWORK Driftctl Scan Result",
+         "channel": "#ts-driftctl-notifications",
+         "text":output
+     }
+     return slack_data
+def slack_webhook(webhook_url):
+    slack_data = slack_notification_content()
+    headers = {
+        'Content-Type': "application/json",
+    }
+    response = requests.post(
+        webhook_url,
+        data=json.dumps(slack_data),
+        headers=headers
+    )
+    if response.status_code == 200:
+        print("Scan result sucessfully sent")
+if __name__ == '__main__':
+    webhook_url = "SLACK_WEBHOOK_URL"
+    slack_webhook(webhook_url)
